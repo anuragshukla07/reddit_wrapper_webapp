f21d66eed25107f7c9f81c63a3508725cc04d28e
anuragshukla07 <anuragshukla07official@gmail.com>
2023-08-24 00:25:58 +0530

    added_frontend

---
+++ reddit_wrapper_project/articles/static/script.js
@@ -0,0 +1,17 @@
+const articlesContainer = document.getElementById('articles');
+
+fetch('/get_articles')
+    .then(response => response.json())
+    .then(articles => {
+        articles.forEach(article => {
+            const articleElement = document.createElement('div');
+            articleElement.className = 'article';
+            articleElement.innerHTML = `
+                <h2>${article.title}</h2>
+                <p>Author: ${article.author}</p>
+                <p>Creation Date: ${new Date(article.creation_date * 1000)}</p>
+                <a href="${article.link}" target="_blank">Read More</a>
+            `;
+            articlesContainer.appendChild(articleElement);
+        });
+    });
