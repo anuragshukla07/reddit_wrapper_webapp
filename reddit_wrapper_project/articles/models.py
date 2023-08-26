3d180782c5ac30a54f65c753671246f8c87cda23
anuragshukla07 <anuragshukla07official@gmail.com>
2023-08-26 17:23:28 +0530

    commented_unnecessary_models_data

--- reddit_wrapper_project/articles/models.py
+++ reddit_wrapper_project/articles/models.py
@@ -1,10 +1,10 @@
-from django.db import models
+# from django.db import models
 
 
-# defines a model to store the article information in the database
-class Articles (models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    creation_date = models.DateTimeField()
-    link = models.URLField()
+# # defines a model to store the article information in the database
+# class Articles (models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     creation_date = models.DateTimeField()
+#     link = models.URLField()
 
